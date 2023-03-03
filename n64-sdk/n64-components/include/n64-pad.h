be0f6ef907ee9e82f0ab409ca32d7cc7beef9275
Joshua Schulz-Andres <126828137+MrJSA@users.noreply.github.com>
2023-03-03 02:22:29 +0100

    Update n64-pad.h

--- n64-sdk/n64-components/include/n64-pad.h
+++ n64-sdk/n64-components/include/n64-pad.h
@@ -7,36 +7,40 @@
 #include "esp_system.h"
 
 // Buttons
-#define BUTTON_A_PIN        GPIO_NUM_27
-#define BUTTON_B_PIN        GPIO_NUM_14
+#define BUTTON_A_PIN        GPIO_NUM_17
+#define BUTTON_B_PIN        GPIO_NUM_5
 
 // DPAD
-#define DPAD_UP_PIN         GPIO_NUM_17
-#define DPAD_DOWN_PIN       GPIO_NUM_15
-#define DPAD_LEFT_PIN       GPIO_NUM_25
-#define DPAD_RIGHT_PIN      GPIO_NUM_26
+#define DPAD_UP_PIN         GPIO_NUM_35
+#define DPAD_DOWN_PIN       GPIO_NUM_33
+#define DPAD_LEFT_PIN       GPIO_NUM_32
+#define DPAD_RIGHT_PIN      GPIO_NUM_25
 
 // Triggers
-#define TRIGGER_L_PIN       GPIO_NUM_23
-#define TRIGGER_R_PIN       GPIO_NUM_21
-#define TRIGGER_Z_PIN       GPIO_NUM_22
+#define TRIGGER_L_PIN       GPIO_NUM_34
+#define TRIGGER_R_PIN       GPIO_NUM_23
+#define TRIGGER_Z_PIN       GPIO_NUM_15
 #define TRIGGER_ZR_PIN      GPIO_NUM_4
 
 // MISC
-#define BUTTON_START_PIN    GPIO_NUM_18
+#define BUTTON_START_PIN    GPIO_NUM_13
 #define BUTTON_HOME_PIN     GPIO_NUM_2
 
 // C-Buttons
-#define C_UP_PIN            GPIO_NUM_13
-#define C_DOWN_PIN          GPIO_NUM_19
-#define C_LEFT_PIN          GPIO_NUM_12
-#define C_RIGHT_PIN         GPIO_NUM_5
+#define C_UP_PIN            GPIO_NUM_22
+#define C_DOWN_PIN          GPIO_NUM_18
+#define C_LEFT_PIN          GPIO_NUM_19
+#define C_RIGHT_PIN         GPIO_NUM_21
 
 // Joystick
-#define JOYSTICK_X_INT_PIN  GPIO_NUM_35
-#define JOYSTICK_X_Q_PIN    GPIO_NUM_34
-#define JOYSTICK_Y_INT_PIN  GPIO_NUM_32
-#define JOYSTICK_Y_Q_PIN    GPIO_NUM_33
+// Joystick X2
+#define JOYSTICK_X_INT_PIN  GPIO_NUM_27
+// Joystick X1
+#define JOYSTICK_X_Q_PIN    GPIO_NUM_26
+// Joystick Y1
+#define JOYSTICK_Y_INT_PIN  GPIO_NUM_14
+// Joystick Y2
+#define JOYSTICK_Y_Q_PIN    GPIO_NUM_12
 
 #define JOYSTICK_MAX_Y      40
 #define JOYSTICK_MAX_X      40
